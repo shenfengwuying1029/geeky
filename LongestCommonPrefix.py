82ffceec33a4d51732cf2d2a47f9ae7bcf4a46c1
DATA_SWAY <swift.wuzhiyuan@gmail.com>
2019-09-30 21:15:29 +0800

    FirstUpdate

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -1,2 +1,6 @@
 def longestCommonPrefix(strs):
-    return 1
+    stepp = min([len(i) for i in strs]) if len(strs) != 0 else  0
+    for i in range(stepp,0,-1):
+        if min([x.find(strs[0][0:i]) for x in strs]) == 0 and max([x.find(strs[0][0:i]) for x in strs]) == 0:
+            return strs[0][0:i]
+    return ''
